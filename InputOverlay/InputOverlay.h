c9d3809f130070c79560506f7e1892232e95f021
tomsLt0 <87011515+tomsLt0@users.noreply.github.com>
2024-12-30 21:22:59 +0200

    Update InputOverlay.h

--- InputOverlay/InputOverlay.h
+++ InputOverlay/InputOverlay.h
@@ -1,11 +1,13 @@
-// InputOverlay.h
 #pragma once
 #include "pch.h"
+
+constexpr auto plugin_version = "1.0";
 
 class InputOverlay : public BakkesMod::Plugin::BakkesModPlugin {
 private:
     bool isKeyPressed[7] = { false }; // For Q,W,E,A,S,D,CapsLock
     bool isMousePressed[2] = { false }; // For LMB, RMB
+    bool showOverlay = true;
 
     void RenderKeyboardOverlay(CanvasWrapper canvas);
     void RenderKey(CanvasWrapper canvas, Vector2F position, const char* label, bool isPressed);
@@ -16,3 +18,5 @@ public:
     virtual void onLoad();
     virtual void onUnload();
 };
+
+extern std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
